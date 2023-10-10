395c22d5f517c184d7ccd4b57af3f83017ecd8b3
sittafatim4 <144087010+sittafatim4@users.noreply.github.com>
2023-10-10 11:53:49 +0700

    Add files via upload

---
+++ WhileGaji12.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class WhileGaji12 {
+   public static void main(String[] args) {
+        Scanner input12 = new Scanner(System.in);
+        int jumlahKaryawan, jumlahJamLembur;
+        double gajiLembur = 0, totalGajiLembur = 0;
+        String jabatan;
+        System.out.print("Masukkan jumlah karyawan: ");
+        jumlahKaryawan = input12.nextInt();
+        int i = 0;
+        while (i < jumlahKaryawan) {
+            System.out.println("Pilihan jabatan - Direktur, Manajer, Karyawan");
+            System.out.print("Masukkan jabatan karyawan ke-" + (i+1) + ": ");
+            jabatan = input12.next();
+            System.out.print("Masukkan jumlah jam lembur: ");
+            jumlahJamLembur = input12.nextInt();
+            i++;
+
+            if (jabatan.equalsIgnoreCase("direktur")) {
+                continue;
+            } else if (jabatan.equalsIgnoreCase("manajer")) {
+                gajiLembur = jumlahJamLembur * 100000;
+            }
+            else if (jabatan.equalsIgnoreCase("karyawan")) {
+                gajiLembur = jumlahJamLembur * 75000;
+            }
+
+            totalGajiLembur += gajiLembur;
+            System.out.println("Total gaji lembur: " + totalGajiLembur);
+        }
+   }
+}
