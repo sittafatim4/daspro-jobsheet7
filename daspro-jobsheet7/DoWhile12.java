b2a0cb661c76370b2cc859b51da7d9e62912a38d
sittafatim4 <144087010+sittafatim4@users.noreply.github.com>
2023-10-10 21:01:25 +0700

    Add files via upload

---
+++ daspro-jobsheet7/DoWhile12.java
@@ -0,0 +1,17 @@
+
+public class DoWhile12 {
+
+    public static void main(String[] args) {
+        int x = 0;
+        do {
+            System.out.println(x);
+        } while (++x <= 8);
+        System.out.println("Program berhenti");
+    }
+}
+/*int x = 10;
+do {
+    System.out.println(x);
+} while (++x <= 8);
+System.out.println("Program berhenti");
+*/
---
+++ daspro-jobsheet7/For12.java
@@ -0,0 +1,9 @@
+
+public class For12 {
+public static void main(String[] args) {
+    for (int i = 1; i <= 5; i++) {
+        System.out.println("Hasil kuadrat dari " + i + " adalah " + (i * i));
+    }
+}
+    
+}
