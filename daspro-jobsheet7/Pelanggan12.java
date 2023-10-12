a26e058594c3a34d59980309954763b90fa8de46
sittafatim4 <144087010+sittafatim4@users.noreply.github.com>
2023-10-12 14:53:20 +0700

    Add files via upload

---
+++ daspro-jobsheet7/Pelanggan12.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Pelanggan12 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String nama;
+    char jawab;
+    int jml = 0;
+    do {
+        System.out.print("Masukkan nama pelanggan: ");
+        nama = sc.nextLine();
+        jml++;
+        System.out.print("Apakah Anda ingin memasukkan nama pelanggan baru (Y/T)? ");
+        jawab = sc.nextLine().charAt(0);
+    } while (jawab == 'y' || jawab == 'Y');
+    System.out.println("Jumlah pelanggan yang Anda masukkan = " + jml);
+}
+}
